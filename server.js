7d4066cce812b90ada96214fb93abadb1fc99b38
Ahmad Kabakibi <ahmadkbakibi@gmail.com>
2016-06-30 14:30:54 +0200

    first code test

---
+++ server.js
@@ -0,0 +1,88 @@
+//Setup web server and socket
+var twitter = require('twitter'),
+    express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+//Setup twitter stream api
+var twit = new twitter({
+  consumer_key: 'aIiEO0MSjEMBcOr7oicfaGFyQ',
+  consumer_secret: 'BEGDT8LGgsctelJkf3RuRxPdDbj5AHV8IbZC2c3dtcGCW9MPWP',
+  access_token_key: '519395465-7R4UKVRCi2MuIJVX2ECW8rttVReXg1XGlwuWAcWo',
+  access_token_secret: 'Tp3g7sOwnNxbvBYdq8reQMVKpIR0DXtCltMFl9dpkLQ0c'
+}),
+stream = null;
+
+//Use the default port (for beanstalk) or default to 8081 locally
+server.listen(process.env.PORT || 8080);
+
+//Setup rotuing for app
+app.use(express.static(__dirname + '/public'));
+
+//Create web sockets connection.
+io.sockets.on('connection', function (socket) {
+
+  socket.on("start tweets", function() {
+
+    if(stream === null) {
+        //Connect to twitter stream passing in filter for entire world.
+        twit.stream('statuses/filter', {'locations':'.734712,52.268002,5.048166,52.454599'}, function(s) {
+            stream = s;
+            stream.on('data', function(data) {
+              // Does the JSON result have coordinates
+              console.log("Data coming :"+JSON.stringify(data));
+              if (data.coordinates){
+                if (data.coordinates !== null){
+                  //If so then build up some nice json and send out to web sockets
+                  var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1]};
+
+                  socket.broadcast.emit("twitter-stream", outputPoint);
+
+                  //Send out to web sockets channel.
+                  socket.emit('twitter-stream', outputPoint);
+                }
+                else if(data.place){
+                  if(data.place.bounding_box === 'Polygon'){
+                    // Calculate the center of the bounding box for the tweet
+                    var coord, _i, _len;
+                    var centerLat = 0;
+                    var centerLng = 0;
+
+                    for (_i = 0, _len = coords.length; _i < _len; _i++) {
+                      coord = coords[_i];
+                      centerLat += coord[0];
+                      centerLng += coord[1];
+                    }
+                    centerLat = centerLat / coords.length;
+                    centerLng = centerLng / coords.length;
+
+                    // Build json object and broadcast it
+                    var outputPoint = {"lat": centerLat,"lng": centerLng};
+                    socket.broadcast.emit("twitter-stream", outputPoint);
+
+                  }
+                }
+              }
+              stream.on('limit', function(limitMessage) {
+                return console.log(limitMessage);
+              });
+
+              stream.on('warning', function(warning) {
+                return console.log(warning);
+              });
+
+              stream.on('disconnect', function(disconnectMessage) {
+                return console.log(disconnectMessage);
+              });
+          });
+      });
+    }
+  });
+
+    // Emits signal to the client telling them that the
+    // they are connected and can start receiving Tweets
+    socket.emit("connected");
+});
+
